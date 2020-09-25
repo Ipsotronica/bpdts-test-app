9badc60d61858f286505e1f59ae33fb734541708
ChrisPeel <ctpeel@gmail.com>
2020-09-25 17:13:29 +0100

    Add test cases to DistanceCalculatorTests and implementation to DistanceCalculator, tests pass

--- src/main/java/org/example/chrispeel/util/DistanceCalculator.java
+++ src/main/java/org/example/chrispeel/util/DistanceCalculator.java
@@ -6,6 +6,22 @@ public class DistanceCalculator {
     //https://www.geeksforgeeks.org/program-distance-two-points-earth/
 
     public static double distanceBetween(double lat1, double long1, double lat2, double long2) {
-        return 0.0;
+        lat1 = Math.toRadians(lat1);
+        long1 = Math.toRadians(long1);
+        lat2 = Math.toRadians(lat2);
+        long2 = Math.toRadians(long2);
+
+        double distanceLat = lat2 - lat1;
+        double distanceLong = long2 - long1;
+
+        double a = Math.pow(Math.sin(distanceLat / 2), 2)
+                + Math.cos(lat1) * Math.cos(lat2)
+                * Math.pow(Math.sin(distanceLong / 2), 2);
+
+        double c = 2 * Math.asin(Math.sqrt(a));
+
+        double r = 3956; //miles
+
+        return c * r;
     }
 }
--- src/test/java/org/example/chrispeel/util/DistanceCalculatorTests.java
+++ src/test/java/org/example/chrispeel/util/DistanceCalculatorTests.java
@@ -5,18 +5,40 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DistanceCalculatorTests {
-    //51.5074° N, 0.1278° W
-    private static final double londonLat = 51.5074;
-    private static final double londonLong = -0.1278;
-
-    //51.2704° N, 0.5227° E
-    private static final double maidstoneLat = 51.2704;
-    private static final double maidstoneLong = 0.5227;
-    //According to Google Maidstone is 32 miles from London
+    //London 51.5074° N, 0.1278° W
+    private static final double LONDON_LAT = 51.5074;
+    private static final double LONDON_LONG = -0.1278;
 
     @Test
-    public void testDistance() {
-        double distance = DistanceCalculator.distanceBetween(londonLat, londonLong, maidstoneLat, maidstoneLong);
-        assertEquals(32.0, distance, 1E-10);
+    public void testDistanceToMaidstone() {
+        //Maidstone 51.2704° N, 0.5227° E
+        final double maidstoneLat = 51.2704;
+        final double maidstoneLong = 0.5227;
+        //Maidstone is ~32 miles from London
+
+        double distance = DistanceCalculator.distanceBetween(LONDON_LAT, LONDON_LONG, maidstoneLat, maidstoneLong);
+        assertEquals(32.0, distance, 1); // delta 1 basically means ignore the decimal
+    }
+
+    @Test
+    public void testDistanceToLeicester() {
+        //Leicester 52.6369° N, 1.1398° W
+        final double leicesterLat = 52.6369;
+        final double leicesterLong = -1.1398;
+        //Leicester is ~89 miles from London
+
+        double distance = DistanceCalculator.distanceBetween(LONDON_LAT, LONDON_LONG, leicesterLat, leicesterLong);
+        assertEquals(89.0, distance, 1); // delta 1 basically means ignore the decimal
+    }
+
+    @Test
+    public void testDistanceToCambridge() {
+        //Cambridge 52.2053° N, 0.1218° E
+        final double cambridgeLat = 52.2053;
+        final double cambridgeLong = 0.1218;
+        //Cambridge is ~49.8 miles from London
+
+        double distance = DistanceCalculator.distanceBetween(LONDON_LAT, LONDON_LONG, cambridgeLat, cambridgeLong);
+        assertEquals(49.0, distance, 1); // delta 1 basically means ignore the decimal
     }
 }
