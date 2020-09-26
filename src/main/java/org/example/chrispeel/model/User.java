1b876fb26d64727518acf018b1ddf46fe92f8c37
ChrisPeel <ctpeel@gmail.com>
2020-09-26 16:24:45 +0100

    Annotate User class with Lombok.Data, and JsonProperty to match the schema of the source data

--- src/main/java/org/example/chrispeel/model/User.java
+++ src/main/java/org/example/chrispeel/model/User.java
@@ -1,13 +1,19 @@
 package org.example.chrispeel.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+@Data
 public class User {
 
     private int id;
+    @JsonProperty("first_name")
     private String firstName;
+    @JsonProperty("last_name")
     private String lastName;
     private String email;
-
+    @JsonProperty("ip_address")
+    private String ipAddress;
     private double latitude;
     private double longitude;
-
 }
